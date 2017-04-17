405efbae221a8d2411244604ce55b93a82f236c6
Diederich Kroeske <dkroeske@gmail.com>
2017-04-17 08:04:51 +0200

    Added simple server

---
+++ simple-server.js
@@ -0,0 +1,46 @@
+var person = require('./person');
+
+var http = require('http');
+
+
+var person_a =
+	{
+		voornaam : 'Diederich',
+		achternaam : 'Kroeske',
+		leeftijd : 48,
+		hobby: 'programmeren'
+	}
+;
+
+// var person_b = [
+// 	{
+// 		voornaam : 'Diederich',
+// 		achternaam : 'Kroeske',
+// 		leeftijd : 48,
+// 		hobby: 'programmeren'
+// 	},
+// 	{
+// 		voornaam : 'Hans',
+// 		achternaam : 'van der Linden',
+// 		leeftijd : 48,
+// 		hobby: 'muziek'
+// 	}
+// ];
+
+
+http.createServer( function(request, response) {
+
+	console.log('REQUEST:' + JSON.stringify(request.headers) );
+
+	//console.log(`REQUEST: ${JSON.stringify(request.headers)}` );
+
+	response.writeHead(200, {
+		'Content-Type' : 'application/json'
+	});
+
+	//response.end( JSON.stringify({user:'diederich', id:6 }) );
+	response.end( JSON.stringify(person.i18l('uk')) );
+
+}).listen(8080);
+
+console.log('All the fun happens on port 8080')
