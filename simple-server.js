87b5a6b8fc70f5b0fe44774fc666f3ba9fabb5b8
Diederich Kroeske <dkroeske@gmail.com>
2017-04-17 09:02:31 +0200

    Work in progress

--- simple-server.js
+++ simple-server.js
@@ -27,11 +27,13 @@ var http = require('http');
 // 	}
 // ];
 
+var port = process.env.PORT || 8080;
+
 
 http.createServer( function(request, response) {
 
-	console.log('REQUEST:' + JSON.stringify(request.headers) );
-
+	console.log('REQUEST:' + JSON.stringify(request.headers));
+	//console.log('REQUEST:' + JSON.stringify(request.headers['user-agent'] ));
 	//console.log(`REQUEST: ${JSON.stringify(request.headers)}` );
 
 	response.writeHead(200, {
@@ -41,6 +43,6 @@ http.createServer( function(request, response) {
 	//response.end( JSON.stringify({user:'diederich', id:6 }) );
 	response.end( JSON.stringify(person.i18l('uk')) );
 
-}).listen(10000);
+}).listen(port);
 
-console.log('All the fun happens on port 1000')
+console.log(`All the fun happens on port ${port}`)
